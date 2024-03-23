5959a387565e19e24c572b278e603ad12a2b1cee
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-03-23 23:14:19 +0600

    basic route added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,18 +1,28 @@
 import { createBrowserRouter } from "react-router-dom"
+import Root from "../Layout/Root"
+import Error from "../Pages/ErrorPage/Error"
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <div>hello world</div>
+        element: <Root />,
+        errorElement: <Error />,
+        children: [
+            {
+                path:'/',
+                element:<div>home</div>
+            },
+            {
+                path:'/donation',
+                element: <h1>Donation...</h1>
+            },
+            {
+                path:'/statistic',
+                element: <h1>statistic...</h1>
+            }
+        ]
     },
-    {
-        path: 'donation',
-        element: <div>donation</div>
-    },
-    {
-        path: '/statistic',
-        element: <div>i am from statistic</div>
-    }
+    
 ])
 
 export default router
